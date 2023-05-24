81a291845d432a91f49dde5d8f7a6698607974f7
Thai Thien Tan <tttan@cusc.ctu.edu.vn>
2023-05-24 14:22:42 +0700

    initial commit

---
+++ DoctorAppointmentSystem/App_Start/BundleConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace DoctorAppointmentSystem
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            //Config ScriptBundle and StyleBundle for jQuery Datatable
+            bundles.Add(new ScriptBundle("~/bundles/datatable").Include(
+                "~/Scripts/jquery.dataTables.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/datatable").Include(
+                "~/Content/jquery.dataTables.min.css"));
+        }
+    }
+}
---
+++ DoctorAppointmentSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DoctorAppointmentSystem.MvcApplication" Language="C#" %>
