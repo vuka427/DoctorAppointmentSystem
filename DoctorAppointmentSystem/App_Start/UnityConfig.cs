26e42b839065f3f06973e07ac439406bbe1d3d78
vuka427 <atrox427@gmail.com>
2023-05-25 13:51:06 +0700

    update file UnityConfig

--- DoctorAppointmentSystem/App_Start/UnityConfig.cs
+++ DoctorAppointmentSystem/App_Start/UnityConfig.cs
@@ -1,15 +1,11 @@
-﻿using DoctorAppointmentSystem.Models.DB;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Data.Entity.Core.Metadata.Edm;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Unity;
-using Unity.AspNet.Mvc;
 
-namespace DoctorAppointmentSystem
+namespace DoctorAppointmentSystem.App_Start
 {
     public static class UnityConfig
     {
