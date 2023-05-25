93be6b8064ddc413d027489e02bb3060bf118756
vuka427 <atrox427@gmail.com>
2023-05-25 10:00:03 +0700

    init dependency injection

--- DoctorAppointmentSystem/Global.asax.cs
+++ DoctorAppointmentSystem/Global.asax.cs
@@ -16,6 +16,7 @@ namespace DoctorAppointmentSystem
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            UnityConfig.RegisterComponents();
         }
     }
 }
