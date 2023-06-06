460fe9245484c28aafaded2c9869243436656f3c
dung-huynh001 <dungb1906640@gmail.com>
2023-06-06 11:10:48 +0700

    Update USER01 06062023 11h10

---
+++ DoctorAppointmentSystem/Menu/MenuItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoctorAppointmentSystem.Menu
+{
+    public class MenuItem
+    {
+        public MenuItem(string id, string title, string icon, bool active, string area, string controller, string action)
+        {
+            this.id = id;
+            this.title = title;
+            this.icon = icon;
+            this.active = active;
+            this.area = area;
+            this.controller = controller;
+            this.action = action;
+        }
+
+        public string id { get; set; }
+        public string title { get; set; }
+        public string icon { get; set; }
+        public bool active { get; set; }
+        public string area { get; set; }
+        public string controller { get; set; }
+        public string action { get; set; }
+
+
+    }
+}
---
+++ DoctorAppointmentSystem/Models/Account/ChangePasswordIO.cs
@@ -0,0 +1,31 @@
+﻿using DoctorAppointmentSystem.Models.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoctorAppointmentSystem.Models.Account
+{
+    public class ChangePasswordIO
+    {
+        LoginIO loginIO = new LoginIO();
+        DBContext dbContext = new DBContext();
+
+        public List<string> GetAuthQuestions(string username)
+        {
+            USER user = loginIO.GetUser(username);
+            List<int?> authQuestionIDList = new List<int?>();
+            List<string> authQuestions = new List<string>();
+            authQuestionIDList.Add(user.PASSWORDRECOVERYQUE1);
+            authQuestionIDList.Add(user.PASSWORDRECOVERYQUE2);
+            authQuestionIDList.Add(user.PASSWORDRECOVERYQUE3);
+            foreach(int id in authQuestionIDList)
+            {
+                SYSTEM_PARA para = dbContext.SYSTEM_PARA.Where(s => s.ID.Equals(id)).FirstOrDefault();
+                string question = para.PARAVAL != "" ? para.PARAVAL : "404! Not found question!";
+                authQuestions.Add(question);
+            }
+            return authQuestions;
+        }
+    }
+}
